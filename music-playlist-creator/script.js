9cfe5f7bd20b8c18a008552cbce786c0c0427a33
Caleb Calderon <calebc4554@meta.com>
2025-06-04 21:47:55 -0700

    finished javascript for opning modal, finished styling modal

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,13 @@
+    const playlist = document.querySelectorAll('.playlist');
+    const modal = document.getElementById('modal');
+
+    playlist.forEach((playlist) => {
+        playlist.addEventListener('click', () => {
+            modal.style.display = "block";
+        });
+    });
+    
+    modal.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
 
