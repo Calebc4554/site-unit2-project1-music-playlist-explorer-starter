52135b1c293deb5a96f31e71b36111f39177c161
Caleb Calderon <calebc4554@meta.com>
2025-06-05 11:42:34 -0700

    finished implementing JS to dynamically update playlist content

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,6 +1,48 @@
     const playlist = document.querySelectorAll('.playlist');
     const modal = document.getElementById('modal');
 
+
+
+fetch('data/data.json')
+    .then(res => res.json())
+    .then(data => {
+
+        const container = document.getElementById("playlist-cards");
+        console.log(container);
+
+        for (const playlist of data.playlists) {
+
+            const playlistCard = document.createElement("div");
+            playlistCard.setAttribute("class", "playlist");
+
+            const playlistImg = document.createElement("img");
+            playlistImg.setAttribute("class", "playlist-img");
+            playlistImg.src = playlist.playlist_art;
+            playlistImg.alt = playlist.playlist_name;
+            playlistCard.appendChild(playlistImg);
+
+            const playlistContent = document.createElement("div")
+            playlistContent.setAttribute("class", "playlist-content")
+            playlistCard.appendChild(playlistContent);
+
+            const playlistName = document.createElement("h2");
+            playlistName.textContent = playlist.playlist_name;
+            playlistContent.appendChild(playlistName);
+
+            const playlistAuthor = document.createElement("p");
+            playlistAuthor.textContent = playlist.playlist_author;
+            playlistContent.appendChild(playlistAuthor);
+
+            const playlistLikes = document.createElement("p");
+            playlistLikes.textContent = playlist.playlist_likes;
+            playlistContent.appendChild(playlistLikes);
+
+            container.appendChild(playlistCard);
+
+            console.log(container);
+        }
+    });
+
     playlist.forEach((playlist) => {
         playlist.addEventListener('click', () => {
             modal.style.display = "block";
@@ -9,40 +51,6 @@
 
     modal.addEventListener('click', () => {
         modal.style.display = "none";
-    });
-
-
-fetch('data/data.json')
-    .then(res => res.json())
-    .then(data => {
-
-        const container = document.getElementById = ("playlist-cards");
-
-        for (const playlist of data.playlists) {
-
-            const playlistCard = document.createElement("div")
-            playlistCard.classList.add = "playlist"
-
-            const playlistImg = document.createElement("img")
-            playlistImg.classList.add = "playlist-img"
-            playlistImg.src = playlist.playlist_art
-            playlistImg.alt = playlist.playlist_name
-            playlistCard.appendChild(playlistImg)
-
-            const playlistName = document.createElement("h2")
-            playlistName.textContent = playlist.playlist_name
-            playlistCard.appendChild(playlistName)
-
-            const playlistAuthor = document.createElement("p")
-            playlistAuthor.textContent = playlist.playlist_author
-            playlistCard.appendChild(playlistAuthor)
-
-            const playlistLikes = document.createElement("p")
-            playlistLikes.textContent = playlist.playlist_likes
-            playlistCard.appendChild(playlistLikes)
-
-            container.appendChild(playlistCard);
-        }
     });
 
 
