5742edc8eb6c0a8dd2d9a40f4f06727876f5f44d
Caleb Calderon <calebc4554@meta.com>
2025-06-05 00:09:23 -0700

    wrote the function to dynamically add playlist using javascript

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -6,8 +6,46 @@
             modal.style.display = "block";
         });
     });
-    
+
     modal.addEventListener('click', () => {
         modal.style.display = "none";
     });
+
+
+fetch('data/data.json')
+    .then(res => res.json())
+    .then(data => {
+
+        const container = document.getElementById = ("playlist-cards");
+
+        for (const playlist of data.playlists) {
+
+            const playlistCard = document.createElement("div")
+            playlistCard.classList.add = "playlist"
+
+            const playlistImg = document.createElement("img")
+            playlistImg.classList.add = "playlist-img"
+            playlistImg.src = playlist.playlist_art
+            playlistImg.alt = playlist.playlist_name
+            playlistCard.appendChild(playlistImg)
+
+            const playlistName = document.createElement("h2")
+            playlistName.textContent = playlist.playlist_name
+            playlistCard.appendChild(playlistName)
+
+            const playlistAuthor = document.createElement("p")
+            playlistAuthor.textContent = playlist.playlist_author
+            playlistCard.appendChild(playlistAuthor)
+
+            const playlistLikes = document.createElement("p")
+            playlistLikes.textContent = playlist.playlist_likes
+            playlistCard.appendChild(playlistLikes)
+
+            container.appendChild(playlistCard);
+        }
+    });
+
+
+
+
 
