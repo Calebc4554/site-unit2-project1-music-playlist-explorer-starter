22e7f2d0b4d097c5802b0e47c7d4aa56331d8041
Caleb Calderon <calebc4554@meta.com>
2025-06-05 16:47:04 -0700

    changed modal text dynamically

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -53,7 +53,15 @@ function openModal(playlist) {
     console.log(playlist);
     const closeButton = document.getElementById('close-button');
     closeButton.addEventListener('click', closeModal);
+    const modalPlaylistTitle = document.getElementById('modal-playlist-title')
+    modalPlaylistTitle.innerHTML = `${playlist.playlist_name}`;
+    const modalPlaylistCreator = document.getElementById('modal-playlist-creator')
+    modalPlaylistCreator.innerHTML = `Created by: ${playlist.playlist_author}`;
+    const mainModalImg = document.getElementById("main-modal-img");
+    mainModalImg.src = playlist.playlist_art;
+    
 }
+
 function closeModal() {
     const modal = document.getElementById('modal');
     modal.style.display = "none";
@@ -65,62 +73,4 @@ function closeModal() {
 
 
 loadPlaylistFromJson();
-
-function loadModalFromJson () {
-    fetch('data/data.json')
-    .then(res => res.json())
-    .then(data => {
-
-        if (data.playlists.length === 0) {
-            alert("Json is Empty");
-            return;
-        }
-
-        const container = document.getElementById("modal");
-
-        for (const playlist of data.playlists) {
-
-            const modalContent= document.querySelector("main-");
-            modalContent.setAttribute("class", "modal-content");
-
-            const mainModal= document.createElement("div");
-            mainModal.setAttribute("class", "main-modal");
-            modalContent.appendChild(mainModal)
-
-            const mainModalImg = document.createElement("img");
-            mainModalImg.setAttribute("class", "");
-            mainModalImg.src = playlist.playlist_art;
-            mainModalImg.alt = playlist.playlist_name;
-            playlistCard.appendChild(playlistImg);
-
-            const playlistContent = document.createElement("div")
-            playlistContent.setAttribute("class", "playlist-content")
-            playlistCard.appendChild(playlistContent);
-
-            const playlistName = document.createElement("h2");
-            playlistName.textContent = playlist.playlist_name;
-            playlistContent.appendChild(playlistName);
-
-            const playlistAuthor = document.createElement("p");
-            playlistAuthor.textContent = playlist.playlist_author;
-            playlistContent.appendChild(playlistAuthor);
-
-            const playlistLikes = document.createElement("p");
-            playlistLikes.textContent = playlist.playlist_likes;
-            playlistContent.appendChild(playlistLikes);
-
-            container.appendChild(playlistCard);
-            console.log(container);
-        }
-        const playlist = document.querySelectorAll('.playlist');
-
-        playlist.forEach((playlist) => {
-            playlist.addEventListener('click', () => {
-                modal.style.display = "block";
-            });
-        });
-    });
-}
-
-loadModalFromJson();
 
